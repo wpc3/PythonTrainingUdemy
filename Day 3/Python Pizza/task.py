45eb4f25b145933087757b50686f287239c66b5c
will chapman <williamchapman53@gmail.com>
2025-03-11 09:23:06 -0400

    solved task

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,25 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+if size == "S":
+    bill = 15
+    if pepperoni == "Y":
+        bill += 2
+    if extra_cheese == "Y":
+        bill += 1
+elif size == "M":
+    bill = 20
+    if pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+elif size == "L":
+    bill = 25
+    if pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+else:
+    print("You chose an invalid size")
+
+print(f"Your final bill is ${bill}")
