59769b38dd9ac50ae5318f16428684c8ce8048a3
will chapman <williamchapman53@gmail.com>
2025-03-12 09:17:27 -0400

    task completed

---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,19 @@
+import random
+import my_module
+
+#numberGenerator = random.randint(0,100)
+#print(numberGenerator)
+#print(my_module.my_favorite_number)
+
+# random_number_0_to_1 = random.random()
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(1,10)
+# print(random_float)
+
+heads_tails = random.randint(0,1)
+
+if heads_tails == 0:
+    print("heads")
+else:
+    print("tails")
