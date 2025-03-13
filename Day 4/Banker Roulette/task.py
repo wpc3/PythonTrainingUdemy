1f528041f0a78e9036e0c939402a0caf5086cd61
will chapman <williamchapman53@gmail.com>
2025-03-13 09:08:18 -0400

    task completed

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -3,21 +3,22 @@ import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 
 randomGenerator = random.randint(0,4)
+print(friends[randomGenerator])
 
-if randomGenerator == 0:
-    print(friends[0])
-
-elif randomGenerator == 1:
-    print(friends[1])
-
-elif randomGenerator == 2:
-    print(friends[2])
-
-elif randomGenerator == 3:
-    print(friends[3])
-
-else:
-    print(friends[4])
+# if randomGenerator == 0:
+#     print(friends[0])
+#
+# elif randomGenerator == 1:
+#     print(friends[1])
+#
+# elif randomGenerator == 2:
+#     print(friends[2])
+#
+# elif randomGenerator == 3:
+#     print(friends[3])
+#
+# else:
+#     print(friends[4])
 
 
 
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,55 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+
+userChoice = int(input("What do you choose? Type 0 for Rock or 1 for Scissors, and 2 for Paper \n"))
+
+computerChoice = random.randint(0,2)
+
+if userChoice == 0:
+    print(rock)
+
+    if computerChoice == 0:
+        print("The computer chose:")
+        print(rock)
+        print("Its a draw brother")
+    elif computerChoice == 1:
+        print("The computer chose:")
+        print(scissors)
+        print("You win brother great choice")
+    else:
+        print("The computer chose:")
+        print("Bad choice brother, you lose")
+
+elif userChoice == 1:
+    print(scissors)
+
+    if computerChoice == 0:
+        print("The computer chose:")
+        print(rock)
+        print("Bad choice brother, you lose")
+    elif computerChoice == 1:
+        print("The computer chose:")
+        print(scissors)
+        print("Its a draw brother")
+    else:
+        print("The computer chose:")
+        print("Great choice brother, you win")
+
+else:
+    print(paper)
+
+    if computerChoice == 0:
+        print("The computer chose:")
+        print(rock)
+        print("You win brother great choice")
+    elif computerChoice == 1:
+        print("The computer chose:")
+        print(scissors)
+        print("Bad choice brother, you lose")
+    else:
+        print("The computer chose:")
+        print("It's a tie brother")
+
+
