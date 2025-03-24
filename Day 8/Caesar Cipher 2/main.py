ffb9db7159bbabaf26802d8e1084b3ce5fad5ea6
will chapman <williamchapman53@gmail.com>
2025-03-24 09:58:59 -0400

    completed task

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -1,14 +1,35 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+def decrypt(original_text, shift_amount):
+    decrypt_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position = shifted_position % len(alphabet)
+        decrypt_text += alphabet[shifted_position]
+    print(f"Here is the decoded result: {decrypt_text}")
+
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+def ceaser():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if direction == "encode":
+        encrypt(original_text= text, shift_amount= shift)
+    elif direction == "decode":
+        decrypt(original_text= text, shift_amount= shift)
+    else:
+        print("Error. Type 'encode' to encrypt, type 'decode' to decrypt")
+        ceaser()
+
+
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
 def encrypt(original_text, shift_amount):
@@ -20,7 +41,7 @@ def encrypt(original_text, shift_amount):
     print(f"Here is the encoded result: {cipher_text}")
 
 
-encrypt(original_text=text, shift_amount=shift)
-
+# encrypt(original_text=text, shift_amount=shift)
+ceaser()
 
 
