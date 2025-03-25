a100a0ea4756ba0bf3c78581074e924ad074c5ec
will chapman <williamchapman53@gmail.com>
2025-03-25 10:50:59 -0400

    completed task

--- Day 9/Nested Lists and Dictionaries/task.py
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -8,3 +8,16 @@ print(travel_log["France"][1])
 nested_list = ["A", "B", ["C", "D"]]
 
 print(nested_list[2][1])
+
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
+
+print(travel_log["Germany"]["cities_visited"][2])
