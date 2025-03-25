d61d347c4e9351f49087f4b8d477885abc461e7b
will chapman <williamchapman53@gmail.com>
2025-03-25 09:55:19 -0400

    completed task

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,13 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again.",
+                          }
+
+print(programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+print(programming_dictionary)
+
+for thing in programming_dictionary:
+    print(thing)
+    print(programming_dictionary[thing])
