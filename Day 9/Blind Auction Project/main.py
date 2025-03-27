a79fe770084e932aa8dbd563745cc22f9252ac66
will chapman <williamchapman53@gmail.com>
2025-03-27 10:32:30 -0400

    figured out how to add to bid dictionary and find highest bid

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -7,3 +7,24 @@ import art
 
 print(art.logo)
 print("Welcome to the secret auction program.")
+
+bidding_dictionary = {}
+
+auction_game = True
+
+while auction_game:
+    name_input = input("What is your name? \n")
+    bid_input = int(input("What is your bid? \n"))
+    bidding_dictionary[name_input] = bid_input
+    adding_bidder_input = input("Would any one else like to bid? y/n \n").lower()
+    # print(bidding_dictionary)
+
+    key_with_max_value = max(bidding_dictionary, key=bidding_dictionary.get)
+    max_value = bidding_dictionary[key_with_max_value]
+
+
+
+
+    # for bid in bidding_dictionary:
+    #     if bidding_dictionary[bid] > highest_bid.values():
+    #         bidding_dictionary = highest_bid
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -2,12 +2,12 @@ programming_dictionary = {"Bug": "An error in a program that prevents the progra
                           "Function": "A piece of code that you can easily call over and over again.",
                           }
 
-print(programming_dictionary["Bug"])
+# print(programming_dictionary["Bug"])
 
 programming_dictionary["Loop"] = "The action of doing something over and over again."
 
-print(programming_dictionary)
+# print(programming_dictionary)
 
 for thing in programming_dictionary:
-    print(thing)
+    # print(thing)
     print(programming_dictionary[thing])
