b4f7fc09164ab4fe55bc9ace89d6c4db6b4196dc
will chapman <williamchapman53@gmail.com>
2025-03-31 10:41:11 -0400

    solved task

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -6,3 +6,18 @@ def format_name(f_name, l_name):
 
 
 print(format_name("wiLL", "cHAPMAN"))
+
+def lijf():
+    def lfjdl():
+        return 0
+    return 2
+
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:
+        return "Great"
+print(my_function(25))
