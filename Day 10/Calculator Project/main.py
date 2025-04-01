609dc0cbd1318d31107f7bb847c49fba2e24a1c4
will chapman <williamchapman53@gmail.com>
2025-04-01 10:50:09 -0400

    made a calculator

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -21,3 +21,52 @@ calc_dictionary["*"] = multiplication
 calc_dictionary["/"] = division
 
 # print(calc_dictionary["*"](4,8))
+
+calc_on = True
+
+while calc_on:
+    first_num = int(input("What is the first number?: "))
+
+    print(" + \n - \n * \n /")
+
+    first_operation_input = input("Pick an operation: ")
+
+    second_num = int(input("What is the second number?: "))
+    result = 0
+
+
+    if first_operation_input == "+":
+        result += calc_dictionary["+"](first_num,second_num)
+        print(result)
+    elif first_operation_input == "-":
+        result += calc_dictionary["-"](first_num,second_num)
+        print(result)
+    elif first_operation_input == "*":
+        result += calc_dictionary["*"](first_num, second_num)
+        print(result)
+    elif first_operation_input == "/":
+        result += calc_dictionary["/"](first_num,second_num)
+        print(result)
+
+    keep_calculating = input(f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation: ").lower()
+    while keep_calculating == "y":
+        print(" + \n - \n * \n /")
+        second_operation = input("Pick an operation: ")
+        new_num = int(input("What is the second number?: "))
+        if second_operation == "+":
+            result = calc_dictionary["+"](result, new_num)
+            print(result)
+        elif second_operation == "-":
+            result = calc_dictionary["-"](result, new_num)
+            print(result)
+        elif second_operation == "*":
+            result = calc_dictionary["*"](result, new_num)
+            print(result)
+        elif second_operation == "/":
+            result = calc_dictionary["/"](result, new_num)
+            print(result)
+        keep_calculating = input(
+            f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation: ").lower()
+    if keep_calculating == "n":
+        result = 0
+
