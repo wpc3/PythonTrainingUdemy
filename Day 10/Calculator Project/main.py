5d2b3784f4c946fc3c9a293d214a628834ad1aa6
will chapman <williamchapman53@gmail.com>
2025-04-01 09:27:25 -0400

    solved task

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,23 @@
+import art
+print(art.logo)
+
 def add(n1, n2):
     return n1 + n2
 
+def subtraction(n1, n2):
+    return n1 - n2
 
+def multiplication(n1, n2):
+    return n1 * n2
+
+def division(n1, n2):
+    return n1 / n2
+
+calc_dictionary = {}
+
+calc_dictionary["+"] = add
+calc_dictionary["-"] = subtraction
+calc_dictionary["*"] = multiplication
+calc_dictionary["/"] = division
+
+# print(calc_dictionary["*"](4,8))
